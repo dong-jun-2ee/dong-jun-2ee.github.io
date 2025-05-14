98e0501e13e27a94079a160e29f94a392b9408b9
dong-jun-2ee <67231009+dong-jun-2ee@users.noreply.github.com>
2025-05-14 07:21:55 +0000

    Deploying to gh-pages from @ dong-jun-2ee/dong-jun-2ee.github.io@34561f50f9b9146595ac34bc6922c07dc7100a26 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
@@ -450,34 +450,34 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_9/";
-            },},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+            },},{id: "projects-on-device-llm-serving-engine",
+          title: 'On Device LLM Serving Engine',
+          description: "Optimizing and supporting language models for execution on NPU backend.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-small-langauge-model",
+          title: 'Small Langauge Model',
+          description: "Training small language models for on-device deployment.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-korean-reasonging-model",
+          title: 'Korean Reasonging Model',
+          description: "Training an o1-like large language model capable of reasoning and responding in Korean.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-domain-adaptation",
+          title: 'Domain Adaptation',
+          description: "Training a large language model capable of understanding domain-specific task.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-vocabulary-expansion",
+          title: 'Vocabulary Expansion',
+          description: "Expanding vocabulary of large language model to include Korean language capabilities.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
+            },},{id: "projects-knowledge-distillation",
+          title: 'Knowledge Distillation',
+          description: "Transferring the capabilities of a large language model to a smaller one.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
             },},{id: "projects-project-7",
